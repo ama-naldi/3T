32c1b742682592bae506f5e33150e6e60c8c648b
ama-naldi <ronaldiguns19@gmail.com>
2025-05-15 13:40:18 +0800

    Add files via upload

---
+++ game 3T - Storyline output/story_content/user.js
@@ -0,0 +1,19 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6LKcTJVClsq":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = dokument.getElementByld('bgSongku');
+audio.src='musik8bit.mp3";
+audio.load0;
+audio.play0;
+audio.volume=0,3;
+}
+
